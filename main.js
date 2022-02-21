6841243f8fcee4811d1d5806debd78acfdfc15a9
rokas <rokasra@gmail.com>
2022-02-21 09:48:50 +0200

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+// console.log('Sveiki visi, hello world')
+//     4 + 4;
+//      console.log(2 + 2 * 5);
+
+// kintamasis naujesnis visada naudosim
+// pasakom kompiuteriui issaugoti vieta atmintyje kur mes noresime saugoti ir naudoti reiksmes
+const x = 5; // Number
+let y = 5;
+let w = "50"; // String
+let isMain = true; //Boolean 1
+let isYellow = false; //Boolean 0
+if (isMain === true) {
+  console.log("it is main");
+}
+let user = {}; //object
+let myArray = []; //object/array
+let isBlue = null;
+let notHere = undefined;
+let blur;
+blur = "sveiki";
+console.log(blur);
+// yra senesnio tipo globalus kintamasis
+var z = 25;
+
+//  x=50; (ismeta klaidas nes x yra constanta)
+// y=100;
+
+console.log("x= " + x + " y = " + y);
+console.log("Sudetis");
+console.log(x + y);
+console.log("Atimtis");
+console.log(x - y);
+console.log("Daugyba");
+console.log(x * y);
+console.log("Dalyba");
+console.log(x / y);
